814ff1b4459e28671324c04479287a4c7fe27b40
sotheaWeb <sotheachhorn239@gmail.com>
2026-03-08 20:16:27 +0700

    edit test

--- src/pages/Careers.jsx
+++ src/pages/Careers.jsx
@@ -6,7 +6,6 @@ import Heros from '../components/Hero';
 import newsImage from '../assets/staff-special.jpg';
 import trip from '../assets/trip.jpg';
 import party from '../assets/party.jpg';
-import yearend from '../assets/yearend.jpg';
 
 const Careers = () => {
 
@@ -38,7 +37,7 @@ const Careers = () => {
     desc: `Spring Party during April 2025 is a big and warm gathering between all teams and have fun together. Work hard and Play harder.`
   };
   const news4 = {
-    image: yearend,
+    image: party,
     title: `Year End Staff Party 2024`,
     desc: `It was a memorable night for KingKong Aviation as the team came together to celebrate the year’s 
           achievements with joy and laughter. Everyone enjoyed gift exchanges, dinner, and recognition for 
